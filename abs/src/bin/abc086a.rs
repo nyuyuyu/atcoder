4381aeff30b0e33b9cc975f633511bc863b8d957
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-09-10 12:35:50 +0000

    [AC] ABC086A

--- abs/src/bin/abc086a.rs
+++ abs/src/bin/abc086a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        ab: [usize; 2],
+    }
+
+    if (ab[0] * ab[1]) % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
 }

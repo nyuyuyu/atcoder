a04d3e5e7ef170642f2e000ed770d905fba09389
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-09-11 13:53:04 +0000

    [AC] ABC081B

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+    }
+
+    let mut count: usize = 0;
+    loop {
+        if a.iter().any(|&i| i % 2 != 0) {
+            break;
+        }
+
+        for i in a.iter_mut() {
+            *i = *i / 2;
+        }
+        count += 1;
+    }
+
+    println!("{}", count);
 }

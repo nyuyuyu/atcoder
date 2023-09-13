9bcbf68cd8e90d52a7c64f58552690248d6a34ae
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-09-13 11:37:11 +0000

    [AC] ABC085C

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,3 +1,45 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        ny: [i32; 2],
+    }
+
+    let n = ny[0];
+    let y = ny[1];
+
+    const YUKICHI_YEN: i32 = 10000;
+    const INAZO_YEN: i32 = 5000;
+    const SOSEKI_YEN: i32 = 1000;
+
+    let mut yukichi_count = -1;
+    let mut inazo_count = -1;
+    let mut soseki_count = -1;
+
+    for i in 0..=(n) {
+        if (YUKICHI_YEN * i) > y {
+            break;
+        }
+
+        for j in 0..=(n - i) {
+            if (YUKICHI_YEN * i + INAZO_YEN * j) > y {
+                break;
+            }
+
+            for k in 0..=(n - i - j) {
+                if i + j + k != n {
+                    continue;
+                }
+
+                if (YUKICHI_YEN * i + INAZO_YEN * j + SOSEKI_YEN * k) == y {
+                    yukichi_count = i;
+                    inazo_count = j;
+                    soseki_count = k;
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("{} {} {}", yukichi_count, inazo_count, soseki_count);
 }

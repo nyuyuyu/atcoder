02dc79d768d2081124b827882e5234ddc2720db3
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-09-13 10:57:21 +0000

    [AC] ABC085B

--- abs/src/bin/abc085b.rs
+++ abs/src/bin/abc085b.rs
@@ -1,3 +1,14 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        d: [usize; n],
+    }
+
+    let steps: HashSet<&usize> = d.iter().collect();
+
+    println!("{}", steps.len());
 }

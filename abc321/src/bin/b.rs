53eac46c6493fc48f680239d03885cadd14f8ec7
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-09-23 13:50:06 +0000

    [AC] ABC321B

--- abc321/src/bin/b.rs
+++ abc321/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        nx: (i32, i32),
+        a: [i32; nx.0 - 1],
+    }
+
+    let x = nx.1;
+
+    let mut answer = -1;
+    for i in 0..=100 {
+        let mut aa = a.clone();
+        aa.push(i);
+
+        let score: i32 = aa.iter().sum();
+        let score = score - (aa.iter().min().unwrap() + aa.iter().max().unwrap());
+        if score >= x {
+            answer = i;
+            break;
+        }
+    }
+
+    println!("{}", answer);
 }

b65b0e09c3d420f383b8f736b478c5a946d312fe
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-09-30 12:45:19 +0000

    [AC] ABC322B

--- abc322/src/bin/b.rs
+++ abc322/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        nm: (usize, usize),
+        s: String,
+        t: String,
+    }
+
+    let n = nm.0;
+    let m = nm.1;
+
+    let mut answer = 3;
+    if &t[..n] == s && &t[(m - n)..] == s {
+        answer = 0;
+    } else if &t[..n] == s {
+        answer = 1;
+    } else if &t[(m - n)..] == s {
+        answer = 2;
+    }
+
+    println!("{}", answer);
 }

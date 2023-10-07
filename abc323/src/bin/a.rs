4310f667fa0e3d7013a03633102783673fbb8f2e
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-10-07 12:21:40 +0000

    [AC] ABC323A

--- abc323/src/bin/a.rs
+++ abc323/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let mut answer = "Yes";
+    for i in (2..=16).step_by(2) {
+        if &s.chars().nth(i - 1).unwrap().to_string() == "1" {
+            answer = "No";
+            break;
+        }
+    }
+
+    println!("{}", answer);
 }

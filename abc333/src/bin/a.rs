62843a2292f4071cbd89c6439836a2a6b1d2501c
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-12-16 12:13:00 +0000

    [AC] ABC333A

--- abc333/src/bin/a.rs
+++ abc333/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let mut answer = String::from("");
+    for _ in 1..=n {
+        answer += &n.to_string();
+    }
+
+    println!("{}", answer);
 }

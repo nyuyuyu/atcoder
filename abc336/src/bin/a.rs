b22f85ff1e15932be9beabb7a17932c437741a84
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-01-14 21:09:19 +0900

    [AC] ABC336A

--- abc336/src/bin/a.rs
+++ abc336/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32
+    }
+
+    let mut ooo = String::new();
+    for _ in 1..=n {
+        ooo += "o";
+    }
+
+    println!("L{}ng", ooo);
 }

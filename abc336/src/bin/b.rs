f6dacba6e7a76f246257dff3a42dfe1b31473dd8
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-01-14 21:23:52 +0900

    [AC] ABC336B

--- abc336/src/bin/b.rs
+++ abc336/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32
+    }
+
+    let mut answer = 0;
+    for c in format!("{:b}", n).chars().rev() {
+        if c == '0' {
+            answer += 1;
+        } else {
+            break;
+        }
+    }
+
+    println!("{}", answer);
 }

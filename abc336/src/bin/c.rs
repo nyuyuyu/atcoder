7272fd4cb27f920bfa8e0e4d2f51a9f1ab7adda3
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-01-15 19:33:35 +0900

    [AC] ABC336C

--- abc336/src/bin/c.rs
+++ abc336/src/bin/c.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut n: u64,
+    }
+
+    if n == 1 {
+        println!("{}", 0);
+        return;
+    }
+
+    n = n - 1;
+
+    let mut answer = String::from("");
+    while n != 0 {
+        answer += &((n % 5) * 2).to_string();
+        n = n / 5;
+    }
+
+    answer = answer.chars().rev().collect::<String>();
+    println!("{}", answer);
 }

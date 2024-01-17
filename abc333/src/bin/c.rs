6f7e05b2b650caea9dfe68c2fc18e7e209561465
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-01-17 16:52:42 +0900

    [AC] ABC333C

--- abc333/src/bin/c.rs
+++ abc333/src/bin/c.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    const REPUNIT_DIGITS: usize = 12;
+
+    let mut v = Vec::new();
+    for i in 1..=REPUNIT_DIGITS {
+        for j in 1..=REPUNIT_DIGITS {
+            for k in 1..=REPUNIT_DIGITS {
+                let mut sum = 0;
+                for x in [i, j, k] {
+                    sum += "1".repeat(x).parse::<u64>().unwrap();
+                }
+
+                v.push(sum);
+            }
+        }
+    }
+
+    v.sort();
+    v.dedup();
+
+    println!("{}", v[n - 1]);
 }

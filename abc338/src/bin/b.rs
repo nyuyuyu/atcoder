69daf05e343d76ddd38e2f0bd2b129327919b6c3
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-01-27 22:16:29 +0900

    [AC] ABC338B

--- abc338/src/bin/b.rs
+++ abc338/src/bin/b.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let mut scores = HashMap::new();
+    for i in 'a'..='z' {
+        scores.insert(i, 0);
+    }
+
+    for i in s.chars() {
+        let score = *scores.get(&i).unwrap();
+        scores.insert(i, score + 1);
+    }
+
+    let mut max = 0;
+    let mut answer = 'a';
+    for i in 'a'..='z' {
+        let score = *scores.get(&i).unwrap();
+        if score > max {
+            max = score;
+            answer = i;
+        }
+    }
+
+    println!("{}", answer);
 }

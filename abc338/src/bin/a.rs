1d7900ebe2be19ff223b78c1c5bfb8a941ecffd1
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-01-27 21:12:48 +0900

    [AC] ABC338A

--- abc338/src/bin/a.rs
+++ abc338/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let a = s.chars().nth(0).unwrap().to_uppercase();
+    let b = &s[1..s.len()].to_lowercase();
+
+    let expected = format!("{}{}", a, b);
+    if s == expected {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

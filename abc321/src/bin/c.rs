a5dc4453574455d386917bb708e3e541ba4189cf
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-02-08 22:30:04 +0900

    [AC] ABC321C

--- abc321/src/bin/c.rs
+++ abc321/src/bin/c.rs
@@ -1,3 +1,23 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        k: usize,
+    }
+
+    let mut answers = vec![];
+    for i in 1..2_u32.pow(10) {
+        let mut num = vec![];
+        for (i, c) in format!("{:b}", i).chars().rev().enumerate() {
+            if c == '1' {
+                num.push(i);
+            }
+        }
+
+        answers.push(num.iter().sorted().rev().join("").parse::<u64>().unwrap());
+    }
+
+    answers.sort();
+    println!("{}", answers[k]);
 }

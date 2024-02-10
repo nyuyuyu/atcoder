286e2f02ba04aad3f4a71c6918e57f5ba99467bb
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-02-10 12:06:52 +0000

    [AC] ABC340A

--- abc340/src/bin/a.rs
+++ abc340/src/bin/a.rs
@@ -1,3 +1,17 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+        d: usize,
+    }
+
+    let mut answer = vec![];
+    for i in (a..=b).step_by(d) {
+        answer.push(i);
+    }
+
+    println!("{}", answer.iter().join(" "));
 }

b831ce175678d56640d9bf0f98a8704cd26984ba
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-02-10 12:24:26 +0000

    [AC] ABC340B

--- abc340/src/bin/b.rs
+++ abc340/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+        query: [[usize; 2]; q],
+    }
+
+    let mut a = vec![];
+    for i in query {
+        if i[0] == 1 {
+            a.push(i[1]);
+            continue;
+        }
+
+        println!("{}", a.iter().nth_back(i[1] - 1).unwrap());
+    }
 }

de2b25c213489d7034c44ce1afecb9791bb4bbff
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-02-11 04:30:00 +0000

    [AC] ABC340C

--- abc340/src/bin/c.rs
+++ abc340/src/bin/c.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u64,
+    }
+
+    let mut memo: HashMap<u64, u64> = HashMap::new();
+    println!("{}", f(n, &mut memo));
+}
+
+fn f(n: u64, m: &mut HashMap<u64, u64>) -> u64 {
+    if n < 2 {
+        return 0;
+    }
+    match m.get(&n) {
+        None => {
+            let sum = f(n / 2, m) + f((n + 1) / 2, m) + n;
+            m.entry(n).or_insert(sum);
+            return sum;
+        }
+        _ => {
+            return m[&n];
+        }
+    }
 }

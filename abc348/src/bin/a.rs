b9f7d869c68262ff475282cbf11343cf78f67887
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-04-06 21:06:29 +0900

    [AC] ABC348A

--- abc348/src/bin/a.rs
+++ abc348/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize
+    }
+
+    let mut answer = String::from("");
+    for i in 1..=n {
+        if i % 3 == 0 {
+            answer.push_str("x");
+        } else {
+            answer.push_str("o");
+        }
+    }
+
+    println!("{}", answer);
 }

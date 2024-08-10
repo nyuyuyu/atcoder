226e78ebb3af649dcbd8a050a8f3edf0a073824b
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-08-10 13:40:32 +0000

    [AC] ABC366B

--- abc366/src/bin/b.rs
+++ abc366/src/bin/b.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let mut max_length = 0;
+    for i in &s {
+        if max_length < i.len() {
+            max_length = i.len();
+        }
+    }
+
+    for i in 0..max_length {
+        let mut line = String::from("");
+        for j in (0..n).rev() {
+            let mut c = s[j].chars().nth(i).unwrap_or(' ');
+            if c == ' ' {
+                for k in (0..j).rev() {
+                    if s[k].chars().nth(i).unwrap_or(' ') != ' ' {
+                        c = '*';
+                        break;
+                    }
+                }
+            }
+            line.push(c);
+        }
+        println!("{}", line);
+    }
 }

7655a7db5b6fda842670fec632e0e6fd755e4b69
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-08-10 12:19:31 +0000

    [AC] ABC366A

--- abc366/src/bin/a.rs
+++ abc366/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        t: usize,
+        a: usize,
+    }
+
+    if t.abs_diff(a) > (n - t - a) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

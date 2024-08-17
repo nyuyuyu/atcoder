aca4c09a3dad98aba108d68c01d26d2e5f243f5e
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2024-08-17 12:47:33 +0000

    [AC] ABC367B

--- abc367/src/bin/b.rs
+++ abc367/src/bin/b.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: String
+    }
+
+    let v: Vec<&str> = x.split('.').collect();
+
+    let mut answer = String::from("");
+    answer.push_str(v[0]);
+
+    let mut flag = false;
+    let mut decimal = String::from("");
+    for i in v[1].chars().rev() {
+        if i == '0' && !flag {
+            continue;
+        } else {
+            flag = true;
+        }
+        decimal.push(i);
+    }
+
+    if decimal.len() > 0 {
+        answer.push('.');
+        for i in decimal.chars().rev() {
+            answer.push(i);
+        }
+    }
+
+    println!("{}", answer);
 }

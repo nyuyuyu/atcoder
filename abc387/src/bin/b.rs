f9eead20e3df8bc04a8dde6ad58e375f73caccf0
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2025-01-04 12:12:35 +0000

    [AC] ABC387B

--- abc387/src/bin/b.rs
+++ abc387/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: i32,
+    }
+
+    let mut sum = 0;
+    for i in 1..=9 {
+        for j in 1..=9 {
+            let answer = i * j;
+            if answer != x {
+                sum += answer;
+            }
+        }
+    }
+
+    println!("{}", sum);
 }

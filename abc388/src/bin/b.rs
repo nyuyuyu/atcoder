53ce9efde71d0f686843cc300a8a0d0e0fbe3688
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2025-01-11 12:38:48 +0000

    [AC] ABC388B

--- abc388/src/bin/b.rs
+++ abc388/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        d: usize,
+        tl: [[usize; 2]; n],
+    }
+
+    let mut answer = Vec::new();
+    for i in 0..d {
+        let mut max = 0;
+        for j in 0..n {
+            let len = tl[j][0] * (tl[j][1] + i + 1);
+            if len > max {
+                max = len;
+            }
+        }
+        answer.push(max);
+    }
+
+    for i in 0..answer.len() {
+        println!("{}", answer[i]);
+    }
 }

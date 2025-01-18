f5b92d4fc17fc2b7eb6ba3244962ce7192ba3ad7
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2025-01-18 12:05:44 +0000

    [AC] ABC389A

--- abc389/src/bin/a.rs
+++ abc389/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let l = s.chars().nth(0).unwrap().to_digit(10).unwrap();
+    let r = s.chars().nth(2).unwrap().to_digit(10).unwrap();
+
+    println!("{}", l * r);
 }

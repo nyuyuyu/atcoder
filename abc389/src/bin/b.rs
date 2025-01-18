30fb22ca753d9079082616a80636d25b95319059
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2025-01-18 12:15:43 +0000

    [AC] ABC389B

--- abc389/src/bin/b.rs
+++ abc389/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut x: usize,
+    }
+
+    let mut answer = 0;
+    for i in 2..usize::MAX {
+        if f(i) == x {
+            answer = i;
+            break;
+        }
+    }
+
+    println!("{}", answer);
+}
+
+fn f(n: usize) -> usize {
+    if n == 1 {
+        1
+    } else {
+        f(n - 1) * n
+    }
 }
